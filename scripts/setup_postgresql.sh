8ab36bcda1957d4e0724fa2d70fea546adc7f137
wmarinho <wmarinho@users.noreply.github.com>
2014-07-19 02:37:29 -0300

    Update setup_postgresql.sh

--- scripts/setup_postgresql.sh
+++ scripts/setup_postgresql.sh
@@ -30,10 +30,10 @@ if [ "$PGHOST" ]; then
 		exit 0
 	fi
 
-	CHK_QUARTZ=`echo "$(psql -U $PGUSER  -h $PGHOST -l | grep quartz | wc -l)"`
-	CHK_HIBERNATE=`echo "$(psql -U $PGUSER  -h $PGHOST -l | grep hibernate | wc -l)"`
-	CHK_JCR=`echo "$(psql -U $PGUSER  -h $PGHOST -l | grep jackrabbit | wc -l)"`
-	CHK_SDATA=`echo "$(psql -U $PGUSER  -h $PGHOST -l | grep sampledata | wc -l)"`
+	CHK_QUARTZ=`echo "$(psql -U $PGUSER  -h $PGHOST -d $PGDATABASE -l | grep quartz | wc -l)"`
+	CHK_HIBERNATE=`echo "$(psql -U $PGUSER  -h $PGHOST -d $PGDATABASE -l | grep hibernate | wc -l)"`
+	CHK_JCR=`echo "$(psql -U $PGUSER  -h $PGHOST -d $PGDATABASE -l | grep jackrabbit | wc -l)"`
+	CHK_SDATA=`echo "$(psql -U $PGUSER  -h $PGHOST -d $PGDATABASE -l | grep sampledata | wc -l)"`
 
 	echo "quartz: $CHK_QUARTZ"
 	echo "hibernate: $CHK_HIBERNATE"
@@ -58,16 +58,16 @@ if [ "$PGHOST" ]; then
 	
 	
 	if [ "$CHK_JCR" -eq "0" ]; then
-	        psql -U $PGUSER  -h $PGHOST -d postgres -f $PENTAHO_HOME/biserver-ce/data/postgresql/create_jcr_postgresql.sql
+	        psql -U $PGUSER  -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/biserver-ce/data/postgresql/create_jcr_postgresql.sql
 	fi
 	if [ "$CHK_HIBERNATE" -eq "0" ]; then
-	        psql -U $PGUSER -h $PGHOST -d postgres -f $PENTAHO_HOME/biserver-ce/data/postgresql/create_repository_postgresql.sql
+	        psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/biserver-ce/data/postgresql/create_repository_postgresql.sql
 	fi
 	if [ "$CHK_QUARTZ" -eq "0" ]; then
-	        psql -U $PGUSER -h $PGHOST -d postgres -f $PENTAHO_HOME/biserver-ce/data/postgresql/create_quartz_postgresql.sql
+	        psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/biserver-ce/data/postgresql/create_quartz_postgresql.sql
 	fi
 	 if [ "$CHK_SDATA" -eq "0" ]; then
-                psql -U $PGUSER -h $PGHOST -d postgres -f $PENTAHO_HOME/biserver-ce/data/postgresql/sampledata.sql
+                psql -U $PGUSER -h $PGHOST -d $PGDATABASE -f $PENTAHO_HOME/biserver-ce/data/postgresql/sampledata.sql
         fi
 	touch /.pentaho_pgconfig
 
