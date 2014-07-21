4912770c519d5c387214e9b20fa71c355eedaf0f
Wellington Marinho <wmarinho@globosat.com.br>
2014-07-20 21:11:40 -0300

    Added cluster config

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,10 @@ if [ ! -f "/.pentaho_pgconfig" ]; then
    sh $PENTAHO_HOME/scripts/setup_postgresql.sh
 fi
 
+if [ "$RDS_HOSTNAME" ]; then
+    HOSTNAME=$(`echo hostname`)
+	sed -i 's/node1/$HOSTNAME/g' $PENTAHO_HOME/biserver-ce/pentaho-solutions/system/jackrabbit/repository.xml
+fi
+
+
 sh $PENTAHO_HOME/biserver-ce/start-pentaho.sh
