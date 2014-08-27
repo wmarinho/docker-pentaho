42f6fbf92e7e8835f6fe4fdd4a03b5c55f46648e
Wellington Marinho <wpmarinho@globo.com>
2014-08-27 00:12:38 -0400

    Added local cluster installer

--- install_cluster.sh
+++ install_cluster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 docker run --name=postgres -d -p 5432:5432 wmarinho/postgresql:9.3
-
-docker run  --name=pentaho01 -d  -p 8080:8080  -e  RDS_PORT=5432  -e RDS_DB_NAME=postgres  -e RDS_USERNAME=pgadmin  -e RDS_HOSTNAME=localhost  -e RDS_PASSWORD=pgadmin. -e DB_VENDOR=postgres wmarinho/pentaho
-
-docker run  --name=pentaho02 -d  -p 8081:8080  -e  RDS_PORT=5432  -e RDS_DB_NAME=postgres  -e RDS_USERNAME=pgadmin  -e RDS_HOSTNAME=localhost  -e RDS_PASSWORD=pgadmin. -e DB_VENDOR=postgres wmarinho/pentaho
+sleep 30 
+docker run  --name=pentaho01 -d  -p 8080:8080 -e INSTALL_PLUGIN=marketplace,cdf,cda,cde,cgg,sparkl,saiku -e  RDS_PORT=5432  -e RDS_DB_NAME=postgres  -e RDS_USERNAME=pgadmin  -e RDS_HOSTNAME=$1  -e RDS_PASSWORD=pgadmin. -e DB_VENDOR=postgres wmarinho/pentaho
+sleep 180 
+docker run  --name=pentaho02 -d  -p 8081:8080 -e INSTALL_PLUGIN=marketplace,cdf,cda,cde,cgg,sparkl,saiku  -e  RDS_PORT=5432  -e RDS_DB_NAME=postgres  -e RDS_USERNAME=pgadmin  -e RDS_HOSTNAME=$1  -e RDS_PASSWORD=pgadmin. -e DB_VENDOR=postgres wmarinho/pentaho
