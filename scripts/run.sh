a0cec6b682f0eac74576cf45e59c60bad426d979
wmarinho <wmarinho@users.noreply.github.com>
2014-12-06 21:22:02 -0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,9 @@
 
 if [ ! -f "/.pentaho_pgconfig" ]; then
    sh $PENTAHO_HOME/scripts/setup_postgresql.sh
-   HOSTNAME=$(`echo hostname`)
-   sed -i "s/node1/node-${HOSTNAME}/g" $PENTAHO_HOME/biserver-ce/pentaho-solutions/system/jackrabbit/repository.xml
+   #HOSTNAME=$(`echo hostname`)
+   
+   sed -i "s/node1/node_${NODE_ID}/g" $PENTAHO_HOME/biserver-ce/pentaho-solutions/system/jackrabbit/repository.xml
    sh $PENTAHO_HOME/scripts/install_plugin.sh
 fi
 
