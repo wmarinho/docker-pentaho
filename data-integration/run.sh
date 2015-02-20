9b9585fdfe820ad115e544aeae28782cbfe1451c
wmarinho <wmarinho@users.noreply.github.com>
2015-02-19 22:45:29 -0200

    Update run.sh

--- data-integration/run.sh
+++ data-integration/run.sh
@@ -9,4 +9,8 @@ if [ "$CARTE_USER" -a "$CARTE_PASS" ]; then
    echo "${CARTE_USER}: $CARTE_PASS" > pwd/kettle.pwd
 fi
 
+if [ -f "./custom_script.sh" ]; then
+   . ./custom_script.sh
+fi
+
 ./carte.sh slave_dyn.xml
