1bc0888ccbee5662b378ad9df3fd9002d99f1d15
techgaun <coolsamar207@gmail.com>
2015-03-15 12:15:35 +0545

    exit on missing argument

--- install_cluster.sh
+++ install_cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <db_hostname>"
+    exit
+fi
 docker run --name=postgres -d -p 5432:5432 wmarinho/postgresql:9.3
 sleep 30 
 docker run  --name=pentaho01 -d  -p 8080:8080 -e INSTALL_PLUGIN=marketplace,cdf,cda,cde,cgg,sparkl,saiku -e  RDS_PORT=5432  -e RDS_DB_NAME=postgres  -e RDS_USERNAME=pgadmin  -e RDS_HOSTNAME=$1  -e RDS_PASSWORD=pgadmin. -e DB_VENDOR=postgres wmarinho/pentaho
