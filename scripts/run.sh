21334fe0772e30a1b427d9b18310b3e94caea79a
Wellington Marinho <wmarinho@users.noreply.github.com>
2015-07-11 16:39:45 -0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
-
 if [ ! -f "/.pentaho_pgconfig" ]; then
    sh $PENTAHO_HOME/scripts/setup_postgresql.sh
    #HOSTNAME=$(`echo hostname`)
-   
+
    sed -i "s/node1/node_${NODE_ID}/g" $PENTAHO_HOME/biserver-ce/pentaho-solutions/system/jackrabbit/repository.xml
+
+fi
+
+if [ ! -f "/.install_plugin" ]; then
    sh $PENTAHO_HOME/scripts/install_plugin.sh
+   touch /.install_plugin
 fi
 
 if [ -f "./custom_script.sh" ]; then
    . ./custom_script.sh
+   mv ./custom_script.sh ./custom_script.sh.bkp
 fi
 
 sh $PENTAHO_HOME/biserver-ce/start-pentaho.sh
