288e7d4ef8cbdcad5b8cdc7ac2d9f28661d7fd44
Wellington Marinho <wpmarinho@globo.com>
2015-07-19 21:02:14 +0000

    .

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -4,6 +4,6 @@ if [ "${INSTALL_PLUGIN}" ]; then
     wget --no-check-certificate 'https://raw.github.com/pmalves/ctools-installer/master/ctools-installer.sh' -P / -o /dev/null
 
     chmod +x ctools-installer.sh
-    ./ctools-installer.sh -s $PENTAHO_HOME/biserver-ce/pentaho-solutions -w $PENTAHO_HOME/biserver-ce/tomcat/webapps/pentaho -b dev -y -c ${INSTALL_PLUGIN}  
+    ./ctools-installer.sh -s $PENTAHO_HOME/biserver-ce/pentaho-solutions -w $PENTAHO_HOME/biserver-ce/tomcat/webapps/pentaho -y -c ${INSTALL_PLUGIN}  
 fi
 
